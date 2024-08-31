33d84b114a42296ef9dd0f38aea577f0dd2c014a
Mamed766 <mehemmeddemirli57@gmail.com>
2024-08-31 18:45:10 +0400

    sanity

--- schemaTypes/post.ts
+++ schemaTypes/post.ts
@@ -9,57 +9,49 @@ export default defineType({
       name: 'title',
       title: 'Title',
       type: 'string',
-    }),
-    defineField({
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'title',
-        maxLength: 96,
-      },
-    }),
-    defineField({
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'},
+      validation: (Rule) =>
+        Rule.required().max(100).warning('Title should not exceed 100 characters.'),
     }),
     defineField({
       name: 'mainImage',
-      title: 'Main image',
+      title: 'Main Image',
       type: 'image',
       options: {
         hotspot: true,
       },
+      validation: (Rule) => Rule.required().warning('Main image is required.'),
     }),
     defineField({
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
-    }),
-    defineField({
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime',
-    }),
-    defineField({
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent',
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+      options: {
+        list: [
+          {title: 'Technology', value: 'Technology'},
+          {title: 'Marketing', value: 'Marketing'},
+          {title: 'Business', value: 'Business'},
+        ],
+        layout: 'radio',
+      },
+      validation: (Rule) => Rule.required().warning('Category is required.'),
     }),
   ],
 
   preview: {
     select: {
       title: 'title',
-      author: 'author.name',
       media: 'mainImage',
+      category: 'category',
     },
     prepare(selection) {
-      const {author} = selection
-      return {...selection, subtitle: author && `by ${author}`}
+      const {title, media, category} = selection
+      return {
+        title,
+        subtitle: category
+          ? category.charAt(0).toUpperCase() + category.slice(1)
+          : 'No category selected',
+        media,
+      }
     },
   },
 })
